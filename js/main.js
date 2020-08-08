3f813060c0f7aebcc1edc39b6dde0b4ea56a9704
Shijun Cao <caosj96@gmail.com>
2020-08-08 16:45:47 -0500

    deploy

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-//$(document).ready(function () {
-(function ($) {
+$(document).ready(function () {
+    //(function ($) {
 
     let $btns = $('.project-area .button-group button');
-
 
     $btns.click(function (e) {
 
@@ -60,5 +59,5 @@
 
     navbarFixed();
 
-    //});
-})(jQuery);
+});
+//})(jQuery);
